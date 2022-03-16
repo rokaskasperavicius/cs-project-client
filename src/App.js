f33f1a7bc0ce9dee5bc5d32330342a2731ce42d2
Rokas Kasperavicius <goodname258@gmail.com>
2022-03-16 11:34:44 +0100

    game

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import { Button } from './components';
+import { useEffect } from "react";
+import { Button } from "./components";
 
-const data = [
-  've',
-  'stuff',
-]
+const data = ["ve", "stuff"];
 
 // http://getbem.com/introduction/
 
 const App = () => {
-  console.log(data)
+  console.log(data);
+
+  // // useEffect(() => {
+  // //   fetch("http://localhost:5000/test")
+  // //     .then((res) => res.json())
+  // //     .then((data) => console.log(data));
+
+  // fetch("http://localhost:5000/test", {
+  //   method: "POST",
+  //   headers: {
+  //     "Content-Type": "application/json",
+  //   },
+  // });
+  // }, []);
 
   return (
     <div className="App">
-      {data.map((value) => (
+      {/* {data.map((value) => (
         <div>{value}</div>
-      ))}
-      <input placeholder=''/>
-      <Button
-        text='My Button'
-      />
+      ))} */}
+      <iframe
+        title="test"
+        src="https://files.crazygames.com/ludo-king/1/index.html"
+        allow="autoplay; fullscreen; microphone; clipboard-read; clipboard-write"
+        webkitallowfullscreen="true"
+        mozallowfullscreen="true"
+        msallowfullscreen="true"
+        allowfullscreen="true"
+        sandbox="allow-forms allow-modals allow-orientation-lock allow-pointer-lock allow-popups allow-presentation allow-scripts allow-same-origin"
+        // style="border: 0px; background-color: rgb(255, 255, 255); width: 10px; height: 10px; min-width: 100%; min-height: 100%;"
+      ></iframe>
+      {/* <input p text="My Button" /> */}
     </div>
   );
-}
+};
 
 export default App;
