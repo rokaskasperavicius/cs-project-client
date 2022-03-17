1417024c6c4ee0bd4f436018f84ca51817aa0dc8
Rokas Kasperavicius <goodname258@gmail.com>
2022-03-17 16:21:40 +0100

    New folders

---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+export const Layout = ({ children }) => (
+  <div
+    style={{
+      // maxWidth: "60vw",
+      padding: "40px",
+      margin: "auto",
+      backgroundColor: "red",
+    }}
+  >
+    {children}
+  </div>
+);
