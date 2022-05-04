cc7d573c1931e5a3fb29243018291da5e0113dae
Tana97 <91907665+Tana97@users.noreply.github.com>
2022-05-04 14:55:00 +0200

    Icon in header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,12 @@
+import homeicon from './Icons/home.svg';
+import React from "react";
+import {Link} from "react-router-dom";
+
 export const Header = () => (
   <header className="header">
     <p className="header__title">CS project</p>
-    <div className="header-product__wrapper"></div>
+    <div className="header-product"></div>
+      <img src={homeicon} width="40" height="40" fill='white' stroke='white'/>
+      <Link to="/" activeStyle></Link>
   </header>
 );
