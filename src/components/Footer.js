5804cd10542c33da469a110f7cdd6c76c8af2621
Tana97 <91907665+Tana97@users.noreply.github.com>
2022-05-04 14:20:22 +0200

    Icons in footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,24 @@
 import React from "react";
 import {Link} from "react-router-dom";
+import mylist from './Icons/mylist.svg';
+import addproduct from './Icons/addproduct.svg';
+//import myprofile from './Icons/myprofile.svg;';
 
 export const Footer = () => (
   <footer className="footer">
     <div className="footer-product__wrapper">
-      <p className="footer-product">My products</p>
-        <Link to="/mylist" activeStyle></Link>
-      <p className="footer-product">Add product</p>
-        <Link to="/addproduct" activeStyle></Link>
-      <p className="footer-product">My profile</p>
-        <p to="/myprofile" activeStyle></p>
+      <p className="footer-product">My products
+          <img src={mylist} width="30" height="30" fill='white' stroke='white'/>
+          <Link to="/mylist" activeStyle></Link>
+      </p>
+      <p className="footer-product">Add product
+          <img src={addproduct} width="30" height="30" fill='white' stroke='white'/>
+          <Link to="/addproduct" activeStyle></Link>
+      </p>
+      <p className="footer-product">My profile
+        <img src={addproduct} width="30" height="30" fill='white' stroke='white'/>
+        <Link to="/myprofile" activeStyle></Link>
+      </p>
     </div>
   </footer>
 );
