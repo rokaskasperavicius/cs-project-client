563866dd020226cf75ed11b90b2e061273898739
Tana97 <91907665+Tana97@users.noreply.github.com>
2022-05-06 12:42:49 +0200

    Background image

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import mylist from "./Icons/mylist.svg";
 import addproduct from "./Icons/addproduct.svg";
-import myprofile from "./Icons/myprofile.svg";
+//import myprofile from "./Icons/myprofile.svg";
 
 export const Footer = () => (
   <footer className="footer">
