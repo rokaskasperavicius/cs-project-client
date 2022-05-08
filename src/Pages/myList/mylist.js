18981ed0a48f47749cab06970eae8097996a143b
Magda <magda.pacan139@gmail.com>
2022-05-08 14:58:49 +0200

    done: search by typing, order by (expiry, name and note), filter by subcategory

--- src/Pages/myList/mylist.js
+++ src/Pages/myList/mylist.js
@@ -1,108 +1,128 @@
 import React, { useEffect, useState } from "react";
 import styles from "../../styles/styles.module.css";
+import DropdownButton from "../../components/DropdownButton";
+import {Input} from "../../components/Input";
+import {apiUrl} from "../../config";
 
-const productsMock = {
-  success: true,
-  data: [
-    {
-      id: 14,
-      name: "carrots",
-      note: "These are really good",
-      expiryDate: "2022-03-05T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-    {
-      id: 34,
-      name: "potatoes",
-      note: "Insert a note here",
-      expiryDate: "2022-06-15T00:00:00.000Z",
-    },
-  ],
-};
 
 export const MyList = () => {
   const [products, setProducts] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [subCategories, setSubCategories] = useState([]);
+  const [selectedCategory, setSelectedCategory] = useState("");
+  const [selectedSubcategory, setSelectedSubcategory] = useState("");
+  const [selectedSearch, setSelectedSearch] = useState("");
+  const [selectedOrderBy, setSelectedOrderBy] = useState("");
+  //TODO: get possible orders from database, MP 08/05
+  const orderBy = ["name","note","expiry"];
 
-  useEffect(() => {
-    if (productsMock.success) {
-      setProducts(productsMock.data);
-    }
+    useEffect(() => {
+        // Fetching all categories
+        fetch(apiUrl + "/categories")
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.success) {
+                    // Set new categories
+                    setCategories(res.data);
+                }
+            });
+        }, []);
+
+    useEffect(() => {
+        // Fail quick
+        if (!selectedCategory) {
+            return;
+        }
+        // Fetch all category's subcategories
+        fetch(apiUrl + "/categories/subcategories?categoryName=" + selectedCategory)
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.success) {
+                    setSubCategories(res.data);
+                }
+            });
+    }, [selectedCategory]);
+
+    useEffect(() => {
+        // Fetching all products
+        fetch(apiUrl + "/products")
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.success) {
+                    // Set new categories
+                    setProducts(res.data);
+                }
+            });
   }, []);
 
-  console.log(products);
+    useEffect(() => {
+        // fail check
+        if (!selectedSubcategory && !selectedSearch && !orderBy) {
+            return;
+        }
+        const parametersArray = [];
+        if (selectedSubcategory) {
+            //TODO: allow filtering by category, MP 08/05
+            parametersArray.push("filter=" + selectedSubcategory)
+        }
+        if (selectedOrderBy) {
+            parametersArray.push("orderby=" + selectedOrderBy)
+        }
+        if (selectedSearch) {
+            parametersArray.push("search=" + selectedSearch)
+        }
+        const endpoint = "/products?" + parametersArray.join('&');
+
+        fetch(apiUrl + endpoint)
+            .then((res) => res.json())
+            .then((res) => {
+                if (res.success) {
+                    setProducts(res.data);
+                }
+            })
+    },[selectedSearch, selectedSubcategory, selectedOrderBy])
+
 
   return (
     <>
       <h3 className={styles.title}>My list</h3>
+        <Input
+            value={selectedSearch}
+            onChange={setSelectedSearch}
+            placeholder="Search for product..."
+        />
+        <DropdownButton
+            placeholder="Select Category"
+            value={selectedCategory}
+            onChange={setSelectedCategory}
+            className={styles.dropdown}
+            data={categories.map((c) => ({
+                value: c.name,
+                label: c.name,
+            }))}
+        />
+
+        <DropdownButton
+            placeholder="Select Subcategory"
+            value={selectedSubcategory}
+            onChange={setSelectedSubcategory}
+            data={subCategories.map((c) => ({
+                value: c.name,
+                label: c.name,
+            }))}
+        />
+        <DropdownButton
+            placeholder="Order By"
+            value={selectedOrderBy}
+            onChange={setSelectedOrderBy}
+            data={orderBy.map((c) => ({
+                value: c,
+                label: c,
+            }))}
+        />
       <table>
         <thead>
           <tr>
-            <th>ID</th>
             <th>Name</th>
             <th>Note</th>
             <th>Expiry Date</th>
@@ -111,7 +131,6 @@ export const MyList = () => {
         <tbody>
           {products.map((p) => (
             <tr>
-              <th>{p.id}</th>
               <th>{p.name}</th>
               <th>{p.note}</th>
               <th>{p.expiryDate}</th>
