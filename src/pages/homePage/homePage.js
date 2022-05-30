13f7b84414f23155a5c5689a68d3850ee6e034fe
Rokas Kasperavicius <goodname258@gmail.com>
2022-05-30 18:29:30 +0200

    Fixing ordering

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import orderBy from "lodash/orderBy";
 
 // Config
 import { apiUrl } from "../../config";
@@ -13,7 +14,7 @@ export const HomePage = () => {
       .then((res) => {
         if (res.success) {
           // Set new categories
-          setProducts(res.data);
+          setProducts(orderBy(res.data, ["expiryDate"]));
         }
       });
   }, []);
@@ -27,32 +28,42 @@ export const HomePage = () => {
   const foodProducts = products.filter((p) => p.categoryName === "Food");
   const medCosProducts = products.filter((p) => p.categoryName !== "Food");
 
-  console.log(foodProducts, medCosProducts);
-
   return (
     <>
       <h2 className="title">Welcome</h2>
       <div className="box">
         <h3>Products expiring soon</h3>
         <div className="box__wrapper">
-          <div>Food</div>
-          {foodProducts
-            .filter((p) => p.expiryDate <= new Date().addDays(1).toISOString())
-            .map((p) => (
-              <div>
-                <span>{p.name}</span>
-                <span>{p.expiryDate.split("T")[0]}</span>
-              </div>
-            ))}
-          <div>Medicine and Cosmetics</div>
-          {medCosProducts
-            .filter((p) => p.expiryDate <= new Date().addDays(30).toISOString())
-            .map((p) => (
-              <div>
-                <span>{p.name}</span>
-                <span>{p.expiryDate.split("T")[0]}</span>
-              </div>
-            ))}
+          {foodProducts.length > 0 && (
+            <>
+              <div style={{ fontWeight: "bold" }}>Food</div>
+              {foodProducts
+                .filter(
+                  (p) => p.expiryDate <= new Date().addDays(1).toISOString()
+                )
+                .map((p) => (
+                  <div>
+                    <span>{p.name}</span>
+                    <span>{p.expiryDate.split("T")[0]}</span>
+                  </div>
+                ))}
+            </>
+          )}
+          {medCosProducts.length > 0 && (
+            <>
+              <div style={{ fontWeight: "bold" }}>Medicine and Cosmetics</div>
+              {medCosProducts
+                .filter(
+                  (p) => p.expiryDate <= new Date().addDays(30).toISOString()
+                )
+                .map((p) => (
+                  <div>
+                    <span>{p.name}</span>
+                    <span>{p.expiryDate.split("T")[0]}</span>
+                  </div>
+                ))}
+            </>
+          )}
         </div>
       </div>
     </>
